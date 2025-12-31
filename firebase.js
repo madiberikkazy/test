025fa7b9d764f2a61f046f62b2836eb60ea26d32
Madi Berikkazy <madi.berikkazy@gmail.com>
2025-12-31 12:32:42 +0500

    third

--- firebase.js
+++ firebase.js
@@ -1,16 +1,19 @@
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
+// Сіз жіберген жаңа конфигурация
 const firebaseConfig = {
   apiKey: "AIzaSyAIxOyHoegDcNozKGrqYpNJksjcIgP6oH8",
   authDomain: "test-946e0.firebaseapp.com",
   projectId: "test-946e0",
-  storageBucket: "test-946e0.appspot.com",
+  storageBucket: "test-946e0.firebasestorage.app", // Түзетілді
   messagingSenderId: "15035507996",
-  appId: "1:15035507996:web:90593830cf1d4f9c6f06d9"
+  appId: "1:15035507996:web:90593830cf1d4f9c6f06d9",
+  measurementId: "G-BGHCR53Q2N"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword };
+export { auth, signInWithEmailAndPassword };
