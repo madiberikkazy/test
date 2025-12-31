221488585e44a71e75ecda27ac562618643ea13a
Madi Berikkazy <madi.berikkazy@gmail.com>
2025-12-31 12:15:57 +0500

    second

--- firebase.js
+++ firebase.js
@@ -1,26 +1,18 @@
-<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.12.2/firebase-auth-compat.js"></script>
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyAIxOyHoegDcNozKGrqYpNJksjcIgP6oH8",
-    authDomain: "test-946e0.firebaseapp.com",
-    projectId: "test-946e0",
-    storageBucket: "test-946e0.appspot.com",
-    messagingSenderId: "15035507996",
-    appId: "1:15035507996:web:90593830cf1d4f9c6f06d9"
-  };
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
+const firebaseConfig = {
+  apiKey: "AIzaSyAIxOyHoegDcNozKGrqYpNJksjcIgP6oH8",
+  authDomain: "test-946e0.firebaseapp.com",
+  projectId: "test-946e0",
+  storageBucket: "test-946e0.appspot.com",
+  messagingSenderId: "15035507996",
+  appId: "1:15035507996:web:90593830cf1d4f9c6f06d9"
+};
 
-  function login() {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+// Firebase initialize
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-    auth.signInWithEmailAndPassword(email, password)
-      .then(() => {
-        window.location.href = "test.html";
-      })
-      .catch(err => alert("Қате: " + err.message));
-  }
-</script>
+export { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword };
