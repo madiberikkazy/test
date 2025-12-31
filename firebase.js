592219bc811932c8d781b0f7eb403a8e3170338d
Madi Berikkazy <madi.berikkazy@gmail.com>
2025-12-31 12:19:48 +0500

    second

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
@@ -11,7 +10,6 @@ const firebaseConfig = {
   appId: "1:15035507996:web:90593830cf1d4f9c6f06d9"
 };
 
-// Firebase initialize
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
